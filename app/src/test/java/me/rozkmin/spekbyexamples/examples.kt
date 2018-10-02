a43101db19c258975036a371d542380d81f0d1d3
Wojciech Zieba <wojciech.zieba@ynd.co>
2018-10-02 11:57:04 +0200

    Adding support for Spek 2
    Adding Spek 2 dependencies
    Reformating code in sample
    Removing duplicated code
    
    Closes #2

--- app/src/test/java/me/rozkmin/spekbyexamples/examples.kt
+++ app/src/test/java/me/rozkmin/spekbyexamples/examples.kt
@@ -1,75 +1,86 @@
 package me.rozkmin.spekbyexamples
 
-import me.rozkmin.spekbdd.BddDsl.And
-import me.rozkmin.spekbdd.BddDsl.Given
-import me.rozkmin.spekbdd.BddDsl.Scenario
-import me.rozkmin.spekbdd.BddDsl.Then
-import me.rozkmin.spekbdd.BddDsl.When
-import me.rozkmin.spekbdd.BddDsl.xAnd
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
-import org.junit.Assert.assertTrue
+import BddDsl.xAnd
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertTrue
 
 class ManyCaseScenarioSample : Spek({
-    Scenario("user is looking for a bus") {
-        //setup context of a user
-        Given("bus list screen") {
+    Feature("looking for a bus") {
+        Scenario("user is looking for a bus") {
+            Given("bus list screen") {
+            }
             When("user exists screen") {
-                Then("save data ") {
-
-                }
+            }
+            Then("save data ") {
+            }
+        }
+        Scenario("user is looking for a bus with favorites") {
+            Given("bust list screen") {
             }
             When("enters bus list screen") {
-                And("has favorite stops") {
-                    Then("display list of bus with favorites on top") {
-                        assertTrue(true)
-                    }
-                }
-                And("has not favorite stops") {
-                    Then("display list of bus") {
-                        assertTrue(true)
-                    }
-                }
             }
+            And("has favorite stops") {
+            }
+            Then("display list of bus with favorites on top") {
+                assertTrue(true)
+            }
+            And("has not favorite stops") {
+            }
+            Then("display list of bus") {
+                assertTrue(true)
+            }
+        }
+        Scenario("user is entering bus detail") {
             When("enters bus detail screen") {
-                Then("display detailed bus stop") {
-                    assertTrue(true)
-                }
+            }
+            Then("display detailed bus stop") {
+                assertTrue(true)
             }
         }
     }
 })
 
-class ManyCaseAndOneDisabledSample : Spek({
-    Scenario("user is looking for a bus") {
-        Given("bus list screen") {
-            When("enters bus list screen") {
-                xAnd("has favorite stops", "NOT IMPLEMENTED YET") {
-                    Then("display list of bus with favorites on top") {
-                        assertTrue(true)
-                    }
-                }
-                And("has not favorite stops") {
-                    Then("display list of bus") {
-                        assertTrue(true)
-                    }
-                }
+class ManyCaseScenarioAndOneDisabledSample : Spek({
+    Feature("looking for a bus") {
+        Scenario("user is looking for a bus") {
+            Given("bus list screen") {
             }
+            When("user exists screen") {
+            }
+            Then("save data ") {
+            }
+        }
+        Scenario("user is looking for a bus with favorites") {
+            Given("bust list screen") {
+            }
+            When("enters bus list screen") {
+            }
+            xAnd("has favorite stops", "NOT IMPLEMENTED") {
+            }
+            Then("display list of bus with favorites on top") {
+                assertTrue(true)
+            }
+            And("has not favorite stops") {
+            }
+            Then("display list of bus") {
+                assertTrue(true)
+            }
+        }
+        Scenario("user is entering bus detail") {
             When("enters bus detail screen") {
-                Then("display detailed bus stop") {
-                    assertTrue(true)
-                }
+            }
+            Then("display detailed bus stop") {
+                assertTrue(true)
             }
         }
     }
 })
-
 
 class VanillaSpek : Spek({
     describe("something") {
-        on("user action") {
+        context("user action") {
             it("should respond to action") {
                 assertTrue(false)
             }
